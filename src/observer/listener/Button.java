61e88fef6720d2d5b3bdf1704bc8eecc20141928
Jeongmin Kim <102003529+N1C0N1C0@users.noreply.github.com>
2023-04-14 22:30:25 +0900

    pattern

---
+++ src/observer/listener/Button.java
@@ -0,0 +1,19 @@
+package observer.listener;
+
+public class Button {
+
+    private String name;
+    private IButtonListener iButtonListener;
+
+    public Button(String name){
+        this.name = name;
+    }
+
+    public void click(String message){
+        iButtonListener.clickEvent(message);
+    }
+
+    public void addListener(IButtonListener iButtonListener){
+        this.iButtonListener = iButtonListener;
+    }
+}
---
+++ src/observer/listener/IButtonListener.java
@@ -0,0 +1,5 @@
+package observer.listener;
+
+public interface IButtonListener {
+    void clickEvent(String event);
+}
